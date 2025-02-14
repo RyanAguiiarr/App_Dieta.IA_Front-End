c857174792662deca5778fd26b694da05bec5064
Ryan Cantareli de Aguiar <165313458+RyanAguiiarr@users.noreply.github.com>
2025-02-14 11:27:25 -0300

    update

--- app/create/index.tsx
+++ app/create/index.tsx
@@ -1,91 +1,107 @@
-import {View, Text, Image, StyleSheet, Pressable, VirtualizedList, ScrollView} from "react-native"
+import { 
+  View, 
+  Text,  
+  StyleSheet, 
+  Pressable, 
+  ScrollView
+ } from 'react-native'
+
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from  'react-hook-form'
 import { colors } from '../../constants/colors'
-import {Header} from "../../components/header"
-import { Input } from "../../components/input"
-import {z} from "zod"
-import {zodResolver} from "@hookform/resolvers/zod"
-import {useForm} from "react-hook-form"
-import {Select} from "../../components/input/select"
-import { useDataStore } from "../../store/data"
-import {router} from "expo-router"
+import { Header } from '@/components/header'
+import { Select } from '../../components/input/select'
+import { useDataStore } from '../../store/data'
+import { router } from 'expo-router'
 
 const schema = z.object({
-  gender: z.string().min(1, {message: "O Sexo é obrigatório"}),
-  objective: z.string().min(1, {message: "O Objetivo é obrigatório"}),
-  level: z.string().min(1, {message: "Selecione seu Nível"}),
+  gender: z.string().min(1, { message: "O sexo é obrigatório"}),
+  objective: z.string().min(1, { message: "O Objetivo é obrigatório"}),
+  level: z.string().min(1, { message: "Selecione seu level"}),
 })
 
 type FormData = z.infer<typeof schema>
 
 export default function Create() {
 
-  
-      const {control, handleSubmit, formState: {errors, isValid}} = useForm<FormData>({
-          resolver: zodResolver(schema)
-      })
+  const { control, handleSubmit, formState: { errors, isValid } } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  })
 
-      const genderOptions = [
-        {label: "Masculino", value: "masculino"},
-        {label: "Feminino", value: "feminino"},
-      ]
-      const levelOptions = [
-        { label: 'Sedentário (pouco ou nenhuma atividade física)', value: 'Sedentário' },
-        { label: 'Levemente ativo (exercícios 1 a 3 vezes na semana)', value: 'Levemente ativo (exercícios 1 a 3 vezes na semana)' },
-        { label: 'Moderadamente ativo (exercícios 3 a 5 vezes na semana)', value: 'Moderadamente ativo (exercícios 3 a 5 vezes na semana)' },
-        { label: 'Altamente ativo (exercícios 5 a 7 dia por semana)', value: 'Altamente ativo (exercícios 5 a 7 dia por semana)' },
-      ]
-      const objectiveOptions = [
-        { label: 'Emagrecer', value: 'emagrecer' },
-        { label: 'Hipertrofia', value: 'Hipertrofia' },
-        { label: 'Hipertrofia + Definição', value: 'Hipertrofia e Definição' },
-        { label: 'Definição', value: 'Definição' },
-      ]
+  const setPageTwo = useDataStore(state => state.setPageTwo)
 
-      const setPageTwo = useDataStore(state => state.setPageTwo)
+  const genderOptions = [
+    { label: "Masculino", value: "masculino" },
+    { label: "Feminino", value: "feminino" },
+  ]
 
-       function handleCreate(data: FormData){
-              console.log("PASSANDO DADOS DA PAGINA 2")
-              setPageTwo({
-                  level: data.level,
-                  gender: data.gender,
-                  objective: data.objective,
-              })
-      
-              router.push("../nutrition")
-          }
-  
+  const levelOptions = [
+    { label: 'Sedentário (pouco ou nenhuma atividade física)', value: 'Sedentário' },
+    { label: 'Levemente ativo (exercícios 1 a 3 vezes na semana)', value: 'Levemente ativo (exercícios 1 a 3 vezes na semana)' },
+    { label: 'Moderadamente ativo (exercícios 3 a 5 vezes na semana)', value: 'Moderadamente ativo (exercícios 3 a 5 vezes na semana)' },
+    { label: 'Altamente ativo (exercícios 5 a 7 dia por semana)', value: 'Altamente ativo (exercícios 5 a 7 dia por semana)' },
+  ]
+
+  const objectiveOptions = [
+    { label: 'Emagrecer', value: 'emagrecer' },
+    { label: 'Hipertrofia', value: 'Hipertrofia' },
+    { label: 'Hipertrofia + Definição', value: 'Hipertrofia e Definição' },
+    { label: 'Definição', value: 'Definição' },
+  ]
+
+
+  function handleCreate(data: FormData){
+    setPageTwo({
+      level: data.level,
+      gender: data.gender,
+      objective: data.objective
+    })
+
+    router.push("/nutrition")
+  }
+
  return (
    <View style={styles.container}>
-    <Header step={"Passo 2"} title={"Finalizando Dieta"}></Header>
-    <ScrollView style={styles.content}> 
+    <Header
+      step='Passo 2'
+      title='Finalizando dieta'
+    />
+
+    <ScrollView style={styles.content}>
       <Text style={styles.label}>Sexo:</Text>
       <Select
-      control={control}
-      name="gender"
-      placeholder="Selecione o seu Sexo..."
-      error={errors.gender?.message}
-      options={genderOptions}
-      />
-      <Text style={styles.label}>Selecione o Nível de Atividade Física:</Text>
-      <Select
-      control={control}
-      name="level"
-      placeholder="Selecione o Nível de Atividade Física..."
-      error={errors.level?.message}
-      options={levelOptions}
-      />
-      <Text style={styles.label}>Selecione o Seu Objetivo:</Text>
-      <Select
-      control={control}
-      name="objective"
-      placeholder="Selecione o Seu Objetivo..."
-      error={errors.objective?.message}
-      options={objectiveOptions}
+        control={control}
+        name="gender"
+        placeholder="Selecione o seu sexo..."
+        error={errors.gender?.message}
+        options={genderOptions}
       />
 
-      <Pressable style={styles.button} onPress={handleSubmit(handleCreate)}>
+      <Text style={styles.label}>Selecione nível de atividade física:</Text>
+      <Select
+        control={control}
+        name="level"
+        placeholder="Selecione o nível de atividade física"
+        error={errors.level?.message}
+        options={levelOptions}
+      />      
+
+      <Text style={styles.label}>Selecione seu objetivo:</Text>
+      <Select
+        control={control}
+        name="objective"
+        placeholder="Selecione o nível de atividade física"
+        error={errors.objective?.message}
+        options={objectiveOptions}
+      />  
+
+      <Pressable 
+        style={styles.button} 
+        onPress={handleSubmit(handleCreate)}
+      >
         <Text style={styles.buttonText}>Avançar</Text>
-      </Pressable>
+      </Pressable>               
 
     </ScrollView>
 
@@ -94,30 +110,30 @@ export default function Create() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.background,
-    },
-    label: {
-      fontSize: 16,
-      color: colors.white,
-      fontWeight: "bold",
-      marginBottom: 8
-    },
-    content: {
-      paddingLeft: 16,
-      paddingRight: 16,
-    },
-    button: {
-      backgroundColor: colors.blue,
-      height: 44,
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: 4
+  container:{
+    flex:1,
+    backgroundColor: colors.background
   },
-  buttonText: {
-      fontSize: 16,
-      color: colors.white,
-      fontWeight: "bold",
+  label:{
+    fontSize: 16,
+    color: colors.white,
+    fontWeight: 'bold',
+    marginBottom: 8, 
+   },
+   content:{
+    paddingLeft: 16,
+    paddingRight: 16,
+   },
+   button:{
+    backgroundColor: colors.blue,
+    height: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  buttonText:{
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: 'bold'
   }
 })
--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://192.168.0.104:3003/create"
+    baseURL: "http://172.20.10.2:3003"
 })
